80fc08e49f7e964f9eb28290a38784f8bbac4f9b
khushbu33 <khushbu.23b0121096@abes.ac.in>
2025-09-18 12:17:06 +0530

    DAY6

---
+++ output.js
@@ -0,0 +1,5 @@
+console.log("Hello from HTML File");
+document.writeln("Hello from HTML File 'writtenln()");
+alert("Hello from HTML File");
+console.error("This is an error message");
+console.warn("This is a warning message");
